2cc90742810b0f4cea002b79d3fe36b150f5acf6
Pantea <panteagh.03@gmail.com>
2025-06-06 13:45:49 +0330

    install framer motion

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,28 +1,41 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import ShowMenuButton from "./ShowMenuButton";
+import { motion } from "framer-motion";
 
-  import Image from "next/image";
-  import React from "react";
-  import ShowMenuButton from "./ShowMenuButton";
-
-  function Hero() {
-    return (
-      <section className="h-96 max-md:h-64 w-full relative">
-        <Image
-          className="-z-1 "
-          src="/assets/HeroImage.jpg"
-          alt="coffee"
-          priority={true}
-          fill={true}
-        />
-        <div className="top-26 text-center relative">
-          <div className="text-amber-950 font-bold text-4xl mb-11">
-         
+function Hero() {
+  return (
+    <section className="h-96 max-md:h-64 w-full relative">
+      <Image
+        className="-z-1 "
+        src="/assets/HeroImage.jpg"
+        alt="coffee"
+        priority={true}
+        fill={true}
+      />
+      <div className="top-26 max-md:top-10 text-center relative">
+        <motion.h1
+          className="text-amber-950 font-bold text-4xl max-md:text-2xl mb-11"
+          initial={{ opacity: 0, y: -40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          viewport={{ once: true, amount: 0.6 }}
+        >
           Taste the real taste of coffee
           <br /> with us!
-          </div>
+        </motion.h1>
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.3, ease: "easeOut" }}
+          viewport={{ once: true, amount: 0.6 }}
+        >
           <ShowMenuButton />
-        </div>
-      </section>
-    );
-  }
+        </motion.div>
+      </div>
+    </section>
+  );
+}
 
-  export default Hero;
+export default Hero;
--- src/components/ShowMenuButton.tsx
+++ src/components/ShowMenuButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ShowMenuButton() {
   return (
-    <button className="bg-orange-950 text-orange-400 border border-orange-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+    <button className="bg-orange-950 text-orange-400 border border-orange-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group max-md:w-">
       <span className="bg-orange-400 shadow-orange-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
       Go To Menu
     </button>
