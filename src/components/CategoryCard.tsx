66d2299834d3cdca74c0d912da5940b3521d6120
Pantea <panteagh.03@gmail.com>
2025-06-09 16:58:56 +0330

    Style: Refine Category Section in home page

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -8,14 +8,15 @@ interface CategoryCardProps {
 
 function CategoryCard({ imgurl, title }: CategoryCardProps) {
   return (
-    <div >
-      <Image
-        className=" -z-10 rounded-br-lg rounded-tl-lg border shadow"
-        src={imgurl}
-        alt={title}
-        width={250}
-        height={250}
-      />
+    <div>
+      <div  className="relative h-[200px] w-[230px] max-md:h-[150px] max-md:w-[150px] max-sm:w-[100px]">
+        <Image
+          className=" -z-10 rounded-br-lg rounded-tl-lg border shadow"
+          src={imgurl}
+          alt={title}
+          fill
+        />
+      </div>
       <h2 className="text-amber-950 font-bold text-center">{title}</h2>
     </div>
   );
--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -1,7 +1,6 @@
-"use client";
 import React from "react";
-import { motion } from "framer-motion";
 import CategoryCard from "./CategoryCard";
+import DivMotionWrapper from "./DivMotionWrapper";
 
 function CategorySection() {
   const categorys = [
@@ -22,27 +21,23 @@ function CategorySection() {
     },
   ];
   return (
-    <motion.div
-      className="container mx-auto my-6 flex flex-col justify-center items-center p-3"
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.8, ease: "easeOut" }}
-      viewport={{ once: true, amount: 0.6 }}
-    >
-      <h1 className="text-amber-950 font-bold text-4xl max-md:text-2xl mb-11">
-        Quick categorization
-      </h1>
+    <DivMotionWrapper>
+      <div className="container mx-auto my-6 flex flex-col justify-center items-center p-3">
+        <h1 className="text-amber-950 font-bold text-4xl max-md:text-2xl mb-11">
+          Quick categorization
+        </h1>
 
-      <div className="grid grid-cols-3 gap-6">
-        {
-            categorys.map((item)=>(
-
-                <CategoryCard key={item.title} imgurl={item.imgurl} title={item.title}/>
-            ))
-        }
-        
+        <div className="grid grid-cols-3 gap-6">
+          {categorys.map((item) => (
+            <CategoryCard
+              key={item.title}
+              imgurl={item.imgurl}
+              title={item.title}
+            />
+          ))}
+        </div>
       </div>
-    </motion.div>
+    </DivMotionWrapper>
   );
 }
 
