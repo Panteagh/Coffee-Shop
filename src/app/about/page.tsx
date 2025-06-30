2779df73d45a17b851e529c549cd21b2a5a86bbe
Pantea <panteagh.03@gmail.com>
2025-06-30 15:41:44 +0330

    Fix: Resolve build error

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import DivMotionWrapper from "@/components/DivMotionWrapper";
 import SocialMediasBottons from "@/components/SocialMediasBottons";
 import React from "react";
@@ -12,7 +11,7 @@ const AboutPage = () => {
         <div className="flex flex-col md:flex-row items-center gap-12">
           <div className="flex-1 text-lg leading-relaxed space-y-6">
             <p>
-              Hi! I'm <strong>Pantea</strong>, a passionate front-end developer
+              Hi! Im <strong>Pantea</strong>, a passionate front-end developer
               who thrives on clean code, elegant UIs, and seamless user
               experiences.
             </p>
@@ -27,8 +26,8 @@ const AboutPage = () => {
               like <strong>Zustand</strong> and <strong>React Query</strong>.
             </p>
             <p>
-              When I’m not coding, you can find me exploring new design systems,
-              reading, or learning new tools. Let’s build something amazing
+              When Im not coding, you can find me exploring new design systems,
+              reading, or learning new tools. Lets build something amazing
               together!
             </p>
 
